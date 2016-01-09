dadb423e8d172872c075ace5ba75f2275f2bff21
root <you@example.com>
2016-01-09 02:06:17 +0000

    Fancy this main function

--- door.py
+++ door.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python
 import RPi.GPIO as GPIO
-def main():
-    PinSwitch = 17
-    with open("status.json", "w") as f:
-        f.write('{ "status" : "boot" }')
-        f.close()
-    while True:
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(PinSwitch, GPIO.IN)
-        input = GPIO.input(PinSwitch)
-        GPIO.wait_for_edge(PinSwitch, GPIO.BOTH)
-        if input == 1:
-            print('on')
-            with open("status.json", "w") as f:
-                f.write('{ "status" : "open" }')
-        else:
-            print('off')
-            with open("status.json", "w") as f:
-                f.write('{ "status" : "closed" }')
-        GPIO.cleanup()
+PinSwitch = 17
+with open("status.json", "w") as f:
+    f.write('{ "status" : "boot" }')
+    f.close()
+while True:
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(PinSwitch, GPIO.IN)
+    input = GPIO.input(PinSwitch)
+    GPIO.wait_for_edge(PinSwitch, GPIO.BOTH)
+    if input == 1:
+        print('on')
+        with open("status.json", "w") as f:
+            f.write('{ "status" : "open" }')
+    else:
+        print('off')
+        with open("status.json", "w") as f:
+            f.write('{ "status" : "closed" }')
+    GPIO.cleanup()
 
