aff0e6f793b42551a1486791edc45067e7d7cd5b
root <you@example.com>
2016-01-09 01:18:09 +0000

    Use variable for pins, change event handling, remove newlines

--- door.py
+++ door.py
@@ -7,48 +7,34 @@ import threading
 from threading import Thread
 
 def server():
-    PORT = 80
+    PORT = 2087
 
     Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
-    httpd = SocketServer.TCPServer(("", PORT), Handler)
+    httpd = SocketServer.TCPServer(("0.0.0.0", PORT), Handler)
 
     print "Serving at port", PORT
     httpd.serve_forever()
 
 def switch():
+    PinSwitch = 17
     with open("status.json", "w") as f:
         f.write('{ "status" : "boot" }')
         f.close()
-
-
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(18, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
-    input = GPIO.input(18)
-
     while True:
-       GPIO.wait_for_edge(18, GPIO.RISING)
-       print('on')
-       with open("status.json", "w") as f:
-           f.write('{ "status" : "open" }')
-
-       GPIO.wait_for_edge(18, GPIO.FALLING)
-       print('off')
-       with open("status.json", "w") as f:
-           f.write('{ "status" : "closed" }')
-
-    GPIO.cleanup()
-
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(PinSwitch, GPIO.IN)
+        input = GPIO.input(PinSwitch)
+        GPIO.wait_for_edge(PinSwitch, GPIO.BOTH)
+        if input == 1:
+            print('on')
+            with open("status.json", "w") as f:
+                f.write('{ "status" : "open" }')
+        else:
+            print('off')
+            with open("status.json", "w") as f:
+                f.write('{ "status" : "closed" }')
+        GPIO.cleanup()
 if __name__ == '__main__':
     Thread(target = server).start()
     Thread(target = switch).start()
 
-
-
-
-#    sock = socket.socket()
-#    sock.connect(('212.47.233.90', 5454))
-#    sock.close()
-##########
-#    sock = socket.socket()
-#    sock.connect(('212.47.233.90', 6464))
-#    sock.close()
