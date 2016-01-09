b6872db0f0ae07e2207cbb7895f6b58a56148db3
root <you@example.com>
2016-01-09 02:02:01 +0000

    Remove webserver, put everything in main function

--- door.py
+++ door.py
@@ -1,21 +1,6 @@
 #!/usr/bin/python
 import RPi.GPIO as GPIO
-import socket
-import SimpleHTTPServer
-import SocketServer
-import threading
-from threading import Thread
-
-def server():
-    PORT = 2087
-
-    Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
-    httpd = SocketServer.TCPServer(("0.0.0.0", PORT), Handler)
-
-    print "Serving at port", PORT
-    httpd.serve_forever()
-
-def switch():
+def main():
     PinSwitch = 17
     with open("status.json", "w") as f:
         f.write('{ "status" : "boot" }')
@@ -34,7 +19,4 @@ def switch():
             with open("status.json", "w") as f:
                 f.write('{ "status" : "closed" }')
         GPIO.cleanup()
-if __name__ == '__main__':
-    Thread(target = server).start()
-    Thread(target = switch).start()
 
