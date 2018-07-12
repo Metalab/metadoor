fc59169f439ba31ede53f6f765567373b2f1f59e
hettipeti <git@thepeko.at>
2018-07-13 00:13:54 +0200

    misc door.py adaption + clarify doorstatus

--- door.py
+++ door.py
@@ -2,6 +2,9 @@
 import RPi.GPIO as GPIO
 
 PinSwitch = 17
+with open("status.json", "w") as f:
+    f.write('{ "status" : "boot" }')
+    f.close()
 while True:
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(PinSwitch, GPIO.IN)
@@ -16,4 +19,6 @@ while True:
         with open("status.json", "w") as f:
             f.write('{ "status" : "closed" }')
     GPIO.cleanup()
+
+
 
