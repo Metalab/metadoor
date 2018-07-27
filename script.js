651d89ed9b07aee49f7313b955a0f80a18648887
Dominik Schramm <1610640051@fh-burgenland.at>
2018-07-27 22:49:29 +0200

    Added time working

--- script.js
+++ script.js
@@ -33,3 +33,45 @@ setInterval(function(){
   xmlhttp.open("GET", "status.json", true);
   xmlhttp.send();
 }, 30000);
+
+// Config Time Since
+const refresh = true;
+const refreshSeconds = 1;
+const origin = new Date("2018-07-24");
+
+// Program
+main();
+function main(){
+  function display(origin){
+    let display = document.querySelector("#timesince-display");
+    display.innerHTML = "Working for: "+ timeSince(new Date(origin).getTime());
+  }
+
+  if(refresh){
+      setInterval(function() {
+          display(origin);
+      }, refreshSeconds*1000);
+  }
+  else
+    display(origin);
+}
+
+function timeSince(origin){
+  function conversion(ms) {
+      var d, h, m, s;
+      s = Math.floor(ms / 1000);
+      m = Math.floor(s / 60);
+      s = s % 60;
+      h = Math.floor(m / 60);
+      m = m % 60;
+      d = Math.floor(h / 24);
+      h = h % 24;
+      return { d: d, h: h, m: m, s: s };
+  };
+
+  let now = Date.now();
+  let timeSince = conversion(now - origin);
+
+  let msg = `${timeSince.d} Days, ${timeSince.h} Hours, ${timeSince.m} Minutes, and ${timeSince.s} seconds.`;
+  return msg;
+}
